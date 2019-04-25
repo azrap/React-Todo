8d63f6042cc752635fa193c06dbb1fddf64ae01d
azrap <azraap@gmail.com>
2019-04-24 22:37:45 -0700

    added event-handler, constructor function and state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,81 @@
 import React from 'react';
+ import Todos from './Todo';
+
+const Todos = [
+  {
+    task: 'do the prework',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'attend live lecture',
+    id: 2,
+    completed: false
+  }
+]
+
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    constructor(){
+
+      super();
+      this.state = {
+        todosOnState: Todos,
+// the below empty strings are what gets populated
+        newTodo: {
+          task:'',
+          id: '',
+          completed: ''
+        }
+
+      }
+
+// This callback event handler function, upon the click of the submit button, updates the todo list with the newly submitted item
+      handleChanges= event => {
+        // console.log(event.target.name);
+        this.setState({
+        todo: {
+          ...this.state.todo, //all of the values on the todo item that might already be there that we don't want to erase
+          [event.target.name]: event.target.value //new inputs of the todo list. 
+        }
+        });
+      }
+    }
+
+ 
+ 
+ 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <div className='todo-list'>
+          <Stj
+        
+        
+        </div>
+
+
+
+
+
+
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+  
+ 
+  
+  
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,110 @@
+import React from 'react';
+
+
+
+
+
+const Todos = [
+    {
+      task: 'do the prework',
+      id: 1,
+      completed: false
+    },
+    {
+      task: 'attend live lecture',
+      id: 2,
+      completed: false
+    }
+  ]
+  
+
+
+
+
+
+
+
+  
+//     {
+//         task: 'get confused AF',
+//         id: 3,
+//         completed: false
+//       },
+  
+//       {
+//         task: 'take a break',
+//         id: 4,
+//         completed: false
+//       },
+  
+//       {
+//         task: 'rewatch lecture',
+//         id: 5,
+//         completed: false
+//       },
+  
+//       {
+//         task: 'feel a little less confused but still pretty confused',
+//         id: 6,
+//         completed: false
+//       },
+  
+  
+//       {
+//         task: 'fork the repo and start the project anyway',
+//         id: 7,
+//         completed: false
+//       },
+  
+  
+//       {
+//         task: 'pair program',
+//         id: 8,
+//         completed: false
+//       },
+  
+//       {
+//         task: 'somehow complete the MVP though no idea how some of the code works',
+//         id: 9,
+//         completed: false
+//       },
+  
+//       {
+//         task: 'pat yourself on the back and celebrate with your team at Standup',
+//         id: 10,
+//         completed: false
+  
+//       },
+  
+//       {
+//         task: 'take a long break ',
+//         id: 11,
+//         completed: false
+        
+//       },
+  
+//       {
+//         task: 'reset the list and do it all over again',
+//         id: 12,
+//         completed: false
+        
+//       }
+  
+//   ];
+  
+Todo = props => {
+{/* add className for styling later */}
+<div> 
+    
+<p>{props.text} </p>
+
+</div>
+
+
+
+}
+
+
+
+
+export default Todos
